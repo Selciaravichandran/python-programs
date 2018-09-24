b3bf2b2af05e0370e59cad6d076c6c52bede39cd
Selciaravichandran <42768555+Selciaravichandran@users.noreply.github.com>
2018-09-24 14:02:09 +0530

    Update and rename Ideone.java to selci1.java

---
+++ selci1.java
@@ -0,0 +1,24 @@
+/*Even or Odd*/
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class selci1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 Scanner reader = new Scanner(System.in);
+ 
+       int num = reader.nextInt();
+ 
+        if(num%2==0)
+            System.out.println("Even");
+        else if(num%3==0)
+            System.out.println("Odd");
+        else
+            System.out.println("invalid");
+    }
+}
+ 
