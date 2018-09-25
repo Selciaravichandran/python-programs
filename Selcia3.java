0e0af9dca0807701c5a02b1a361204e0a33b8899
Selciaravichandran <42768555+Selciaravichandran@users.noreply.github.com>
2018-09-25 09:38:07 +0530

    Create Selcia3.java

---
+++ Selcia3.java
@@ -0,0 +1,26 @@
+ /*Alphabet or No*/
+ 
+ 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Selcia3
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+ 
+          char ch;
+        Scanner scan = new Scanner(System.in);
+         ch = scan.next().charAt(0);
+ if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z'))
+        {
+            System.out.print("Alphabet");
+        }
+        else
+        {
+            System.out.print( "No");
+        }
+    }
+}
